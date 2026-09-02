2dd536324a001dd1601fcf153ca7edece9df67ce
priyanshu ahir <priyanshuahir777@gmail.com>
2026-09-02 13:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-//your JS code here. If required.
-const code = document.querSelectorAll(".code");
+const codes = document.querySelectorAll(".code");
 
-const.forEach((code, index)=>{
-	code.addEventListener("input", (e)=>{
-		e.target.value = e.target.value.replace(/\D/g,"");
+codes.forEach((code, index) => {
+  code.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/\D/g, "");
 
-		if(e.target.value != "" && index<codes.length-1){
-			code[index+1].focus();
-		}
-	});
+    if (e.target.value !== "" && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
 
-	code.addEventListner("keydown",(e)=>{
-		if(e.key == "backspace"){
-			if(code.value != ""){
-				code.value="";
-			}
-			else if(index>0){
-				codes[index-1].value="";
-				codes[index-1].focus();
-			}
-		}
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value !== "") {
+        code.value = "";
+      }
+      else if (index > 0) {
+        codes[index - 1].value = "";
+        codes[index - 1].focus();
+      }
+    }
 
-		if(!/^\d$/.test(e.key) &&
+    if (
+      !/^\d$/.test(e.key) &&
       e.key !== "Backspace" &&
       e.key !== "Delete" &&
       e.key !== "ArrowLeft" &&
       e.key !== "ArrowRight" &&
-      e.key !== "Tab"){
-			e.preventDefault();
-	  }
-	});
+      e.key !== "Tab"
+    ) {
+      e.preventDefault();
+    }
+  });
 });
